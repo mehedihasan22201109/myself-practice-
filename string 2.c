f900b37fc9ea251019b387fb07183b54572cd6d3
Mehedi Hasan <140379801+mehedihasan22201109@users.noreply.github.com>
2023-09-20 22:28:06 +0600

    Add files via upload

---
+++ string 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    char name []="MEHEDIHASAN";
+    int i=0;
+    while (name [i]!='\0')
+    {
+        printf ("%c\n",name[i]);
+        i++;
+    }
+    return 0;
+}
