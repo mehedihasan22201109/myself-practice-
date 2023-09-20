ce12eb52670c8e8886d225e239ddab0216834165
Mehedi Hasan <140379801+mehedihasan22201109@users.noreply.github.com>
2023-09-20 22:27:13 +0600

    Add files via upload

---
+++ string 1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main ()
+{
+    char name [7]={'M','E','H','E','D','I'};
+    printf ("my name is %s\n",name);
+  char nm []={"mehedi hasan hridoy"};
+  printf ("my name is %s",nm);
+  return 0;
+}
